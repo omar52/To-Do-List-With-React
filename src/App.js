8cf1b498abfe16e95153c5d71e68a353ff084fa1
Omar 52 <omarrabdoo00@gmail.com>
2024-06-22 14:57:23 +0300

    add some enhancements

--- src/App.js
+++ src/App.js
@@ -1,57 +1,84 @@
-
 import Button from "react-bootstrap/Button";
+
+import React from "react";
 import Form from "react-bootstrap/Form";
 import "./App.css";
 import { useState } from "react";
 import { Alert } from "bootstrap";
 import Tasks from "./components/form/Tasks";
 
-
 function App() {
   const [tasks, setTasks] = useState([]);
+  const [inputValue, setInputValue] = useState("");
 
+  // const handleSubmit = (event) => {
+  //   event.preventDefault();
+  //   const formData = new FormData(event.target);
+  //   const newTask = formData.get("task");
+  //   if (newTask && newTask.length > 4) {
+  //     // console.log(formData.get("task"));
+  //     setTasks([...tasks, newTask]);
+  //     console.log(tasks);
+  //     event.target.reset();
+  //   } else {
+  //     new Alert(`U Need To Add a Valid Task `);
+  //   }
+  // };
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    const formData = new FormData(event.target);
-    const newTask = formData.get("task");
-    if(newTask && newTask.length > 4){
+    const newTask = inputValue;
+
+    if (newTask && newTask.length > 4) {
       // console.log(formData.get("task"));
       setTasks([...tasks, newTask]);
       console.log(tasks);
-    }else{
-      throw Alert(`U Need To Add a Valid Task `)
-    };
+      setInputValue("");
+    } else {
+      new Alert(`U Need To Add a Valid Task `);
     }
+  };
 
-
-  const handleClick =(event)=>{
-    event.target.style.textDecoration = "line-through"; 
-    event.target.style.color = "green"; 
-  }
-  
-  const handleTaskClick =(event)=>{
-    event.target.parentElement.remove();
-  }
+  const handleDeleteTask = (index) => {
+    const updatedList = tasks.filter(
+      (task, filterIndex) => filterIndex !== index
+    );
+    setTasks(updatedList);
+  };
 
   return (
     <div className="bg-secondary container rounded mt-5 App">
+      
       <Form onSubmit={handleSubmit} className="px-5 pb-1  pt-5 text-center">
         <Form.Group className="mb-3" controlId="formBasicEmail">
           <Form.Label style={{ fontSize: "30px" }}>
-            Enter What do u wand to do!!!
+            Enter What Do You Want To Do
           </Form.Label>
-          <Form.Control name="task" type="text" placeholder="Enter Task" />
+          <Form.Control
+            onChange={(event) => setInputValue(event.target.value)}
+            name="task"
+            type="text"
+            placeholder="Enter Task"
+            value={inputValue}
+          />
         </Form.Group>
         <Button className="mt-2" variant="light" type="submit">
           Add Task
         </Button>
-        <ul className="tasks-group d-flex flex-column mb-3 mt-2 justify-content-center ps-0 mx-5">
-          {tasks.map((task) => <>
-            <Tasks task={task} handleClick={handleClick} handleTaskClick={handleTaskClick} />
-          </>
-          )}
-        </ul>
+        {!tasks.length ? (
+          <p className=" mt-3 text-white">" There Are No Tasks To Do "</p>
+        ) : (
+          <ul className="tasks-group d-flex flex-column mb-3 mt-2 justify-content-center ps-0 mx-5">
+            {tasks.map((task, index) => (
+              <React.Fragment key={index}>
+                <Tasks
+                  task={task}
+                  handleTaskClick={() => handleDeleteTask(index)}
+                />
+              </React.Fragment>
+            ))}
+          </ul>
+        )}
       </Form>
     </div>
   );
--- src/components/form/Tasks.js
+++ src/components/form/Tasks.js
@@ -1,13 +1,22 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-
-
 import { faXmark } from "@fortawesome/free-solid-svg-icons";
-const Tasks =({task ,handleClick , handleTaskClick})=>{
+import { useState } from "react";
+
+
+const Tasks =({task  , handleTaskClick })=>{
+
+    const [done , setDone] = useState(false)
+
+    const handleClick =()=>{
+        setDone(!done);
+    }
+
     return(
         <div className="text-center li-div d-flex justify-content-between align-items-center bg-white px-3 py-2 mt-1 rounded mb-3">
-              <li onClick={handleClick} style={{ cursor: "pointer" ,color :"red" }} className="bg-white">{task}</li>
+              <li onClick={handleClick} style={{ cursor: "pointer" , color : done? "green" : "red" , textDecoration: done ? "line-through": "none" }} className="bg-white">{task}</li>
               <FontAwesomeIcon onClick={handleTaskClick} style={{ cursor: "pointer" }} icon={faXmark} />
-            </div>
+        </div>
     )
 }
 export default Tasks
+
